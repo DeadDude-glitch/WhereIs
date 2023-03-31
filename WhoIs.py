bca742a477834ac5d950a07f45fc54b100fb62e3
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2023-03-31 15:27:43 +0200

    handled unexpected json format response
    since I did not handle all the responses I decide to show the raw response to user

--- WhoIs.py
+++ WhoIs.py
@@ -1,4 +1,3 @@
-
 import requests
 import colorama
 from ipaddress import ip_address
@@ -14,25 +13,30 @@ key = "" # <--- KEY HERE
 # ADD YOUR KEY HERE
 
 
-# just a habit not neccesary 
+# just a habit not neccesary
 if __name__ == "__main__" and key != "":
     from sys import argv
-    
     # show help message
     if len(argv) > 1: address = argv[1]
-    else: 
+    else:
         print(colorama.Fore.RED + "[!] missing IP address")
         print(colorama.Fore.YELLOW+"SYNTAX: python3 Whereis.py {ip1} [ipaddress2]...")
         quit()
-    
+
     # loop on ip addresses
     for address in argv[1:]:
-        try: 
+        try:
             # reading/validating user input
             ip_address(address)
             # request from API
             response = requests.get(f"{URL}?apiKey={key}&ip={address}").json()
-            
-            message = colorama.Fore.GREEN + response['ip'] + ' is in ' + response["country_name"]+ ' using ' + response["isp"] + " services"
+            try:
+                # shown output message
+                message = colorama.Fore.GREEN + response['ip'] + ' is in ' + response["country_name"]+ ' using ' + response["isp"] + " services"
+            except KeyError:
+                message = f"{colorama.Fore.RED} [!] unexpected JSON format"
+                print(f"raw response:\n{response}")
+        # not valid IP addresses case
         except ValueError: message = f"{colorama.Fore.RED}{address} is not a valid IPv4 or IPv6 address"
+        # present output
         print(message)
